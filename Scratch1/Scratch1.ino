f3ff52fb7754e4ec941054353b67a79e0a5149ec
Youssef Zayed <yzaye1@ocdsb.ca>
2018-05-07 18:33:34 -0400

    added left and right turns

--- Scratch1/Scratch1.ino
+++ Scratch1/Scratch1.ino
@@ -12,31 +12,15 @@ const int left_mot_fwd_speed = 75, right_mot_fwd_speed = 75;
 const int correct_mot_speed = 100;
 
 void setup() {
-
+  
 }
 
 void loop() {
-  
-  if ((irL.isBlack() == 0) && (irR.isBlack() == 0)) {
-    motL.forward(left_mot_fwd_speed);
-    motR.forward(right_mot_fwd_speed);
-  }
-
-  else if ((irL.isBlack() == 1) && (irR.isBlack() == 0)) {
-    motL.forward(correct_mot_speed);
-    motR.forward(right_mot_fwd_speed);
-  }
-
-  else if ((irL.isBlack() == 0) && (irR.isBlack() == 1)) {
-    motL.forward(left_mot_fwd_speed);
-    motR.forward(correct_mot_speed);
-  }
-
-  else if ((irL.isBlack() == 1) && (irR.isBlack() == 1)) {
+  centering();
+   if ((irL.isBlack() == 1) && (irR.isBlack() == 1)) {
     stop();
     turnLeft();
   }
-  
 }
 
 void turnLeft() {
@@ -53,9 +37,29 @@ void turnLeft() {
 }
 
 void kickLeft() {
-  while (irR.isBlack()) {
+  while (irL.isBlack()) {
     motL.forward(0);
     motR.forward(right_mot_fwd_speed);
+  }
+}
+
+void turnRight() {
+  int turnCounter = 0;
+  kickRight();
+  while (turnCounter < 2) {
+    motR.forward(0);
+    motL.forward(right_mot_fwd_speed);
+    if (irR.isBlack()) {
+      kickRight();
+      turnCounter++;
+    }
+  }
+}
+
+void kickLeft() {
+  while (irR.isBlack()) {
+    motR.forward(0);
+    motL.forward(right_mot_fwd_speed);
   }
 }
 
@@ -65,4 +69,22 @@ void stop() {
     motR.halt();
   }
 }
+void centering(){
+    
+  if ((irL.isBlack() == 0) && (irR.isBlack() == 0)) {
+    motL.forward(left_mot_fwd_speed);
+    motR.forward(right_mot_fwd_speed);
+  }
 
+  else if ((irL.isBlack() == 1) && (irR.isBlack() == 0)) {
+    motL.forward(correct_mot_speed);
+    motR.forward(right_mot_fwd_speed);
+  }
+
+  else if ((irL.isBlack() == 0) && (irR.isBlack() == 1)) {
+    motL.forward(left_mot_fwd_speed);
+    motR.forward(correct_mot_speed);
+  }
+
+ 
+  }
