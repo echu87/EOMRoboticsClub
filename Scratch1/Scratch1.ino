87ec71689899ae24f70141b4b2564acfe56485d5
Youssef Zayed <yzaye1@ocdsb.ca>
2018-05-07 21:04:36 -0400

    fixing bug

--- Scratch1/Scratch1.ino
+++ Scratch1/Scratch1.ino
@@ -7,7 +7,9 @@ IRControl irR(1);
 MotorControl motL(1);
 MotorControl motR(2);
 UltrasonicControl sonic (13,12);
-   
+ 
+boolean position[7][4];
+int dir, posX,posY;
 void setup() 
 {
     Serial.begin(9600);
@@ -23,7 +25,14 @@ void loop()
   Serial.print("\t");
   Serial.println(sonic.getDistance());
   
-  if (irL.isBlack() == 0 && irR.isBlack() == 0)
+  centering();
+ 
+}
+
+
+void centering(){
+  
+ if (irL.isBlack() == 0 && irR.isBlack() == 0)
   {
     motL.forward(90);
     motR.forward(90);
@@ -41,38 +50,23 @@ void loop()
     motR.forward(120);
   }
   
-   else if ((irL.isBlack() == 1) && (irR.isBlack() == 1))  //hit and intersection and stop!
-  {
-    for (long i=0; i<10000 ; i++)  //Note: Needs to be long for 4 byte storage
-    {                               //Rather than 2 byte storage of int
-      motL.halt();
-      motR.halt();
-    }
-
-    for (long j=0; j<20000; j++)
-    {
-      motL.forward(120);
-      motR.forward(0);
-    }
-  }
+//   else if ((irL.isBlack() == 1) && (irR.isBlack() == 1))  //hit and intersection and stop!
+//  {
+//    for (long i=0; i<10000 ; i++)  //Note: Needs to be long for 4 byte storage
+//    {                               //Rather than 2 byte storage of int
+//      motL.halt();
+//      motR.halt();
+//    }
+//
+//    for (long j=0; j<20000; j++)
+//    {
+//      motL.forward(120);
+//      motR.forward(0);
+//    }
+//  }
  
-}
+  }
 
-
-//#include <IRControl.h>
-//#include <MotorControl.h>
-//#include <UltrasonicControl.h>
-//
-//IRControl irL(0);
-//IRControl irR(1);
-//MotorControl motL(1);
-//MotorControl motR(2);
-//UltrasonicControl sonic(13,12);
-//boolean position[7][4];
-//int dir, posX,posY;
-//const int left_mot_fwd_speed = 0, right_mot_fwd_speed = 75;
-//const int correct_mot_speed = 100;
-//
 //void setup() {
 //  for(int y =0;y<7;y++){
 //    for(int x =0;x<7;x++){
@@ -179,24 +173,4 @@ void loop()
 //    motR.halt();
 //  }
 //}
-//void centering(){
-//  
-//  if (irL.isBlack() == 0 && irR.isBlack() == 0)
-//  {
-//    motL.forward(90);
-//    motR.forward(90);
-//  }
-//  
-//  else if ((irL.isBlack() == 1) && (irR.isBlack() == 0)) //moving left
-//  {
-//    motL.forward(120);  //turn up left wheel to correct to the right
-//    motR.forward(70);
-//  }
-//
-//   else if ((irL.isBlack() == 0) && (irR.isBlack() == 1))//moving right
-//  {
-//    motL.forward(70);  //turn up the right wheel to correct to the left
-//    motR.forward(120);
-//  }
-// 
-//  }
+
