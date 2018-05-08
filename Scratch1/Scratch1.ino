2450ce629d78c070f939644d5106afdd7f1e0b31
Youssef Zayed <yzaye1@ocdsb.ca>
2018-05-07 21:27:33 -0400

    fixing bug

--- Scratch1/Scratch1.ino
+++ Scratch1/Scratch1.ino
@@ -7,21 +7,13 @@ IRControl irR(1);
 MotorControl motL(1);
 MotorControl motR(2);
 UltrasonicControl sonic (13,12);
- 
+
 boolean position[7][4];
-int dir, posX,posY;
+int dir, posX,posY,right_mot_fwd_speed =120;
 void setup() 
 {
     Serial.begin(9600);
       delay(5000);
-       for(int y =0;y<7;y++){
-    for(int x =0;x<7;x++){
-      position[x][y] = false;
-      }
-    }
-    position[0][0] = true;
-    dir = 1;
-    posX=0,posY=0;
 }
 
 
@@ -32,12 +24,13 @@ void loop()
   Serial.print(irR.isBlack());
   Serial.print("\t");
   Serial.println(sonic.getDistance());
-
- moving();
+  
+  centering();
+ 
 }
 
 
-void moving(){
+void centering(){
   
  if (irL.isBlack() == 0 && irR.isBlack() == 0)
   {
@@ -56,16 +49,59 @@ void moving(){
     motL.forward(70);  //turn up the right wheel to correct to the left
     motR.forward(120);
   }
-
-//   else if ((irL.isBlack() == 1) && (irR.isBlack() == 1))  //hit and intersection and stop!
-//  {
-//   turnLeft();
-//  }
+  
+   else if ((irL.isBlack() == 1) && (irR.isBlack() == 1))  //hit and intersection and stop!
+  {
+    turnLeft();
+  }
  
   }
 
-
-
+//void setup() {
+//  for(int y =0;y<7;y++){
+//    for(int x =0;x<7;x++){
+//      position[x][y] = false;
+//      }
+//    }
+//    position[0][0] = true;
+//    dir = 1;
+//    posX=0,posY=0;
+//    
+//}
+//
+//void loop() {
+//if (irL.isBlack() == 0 && irR.isBlack() == 0)
+//  {
+//    motL.forward(90);
+//    motR.forward(90);
+//  }
+//  
+//  else if ((irL.isBlack() == 1) && (irR.isBlack() == 0)) //moving left
+//  {
+//    motL.forward(120);  //turn up left wheel to correct to the right
+//    motR.forward(70);
+//  }
+//
+//   else if ((irL.isBlack() == 0) && (irR.isBlack() == 1))//moving right
+//  {
+//    motL.forward(70);  //turn up the right wheel to correct to the left
+//    motR.forward(120);
+//  }
+//  
+//   else if ((irL.isBlack() == 1) && (irR.isBlack() == 1))  //hit and intersection and stop!
+//  {
+//    for (long i=0; i<10000 ; i++)  //Note: Needs to be long for 4 byte storage
+//    {                               //Rather than 2 byte storage of int
+//      motL.halt();
+//      motR.halt();
+//    }
+//
+//    for (long j=0; j<20000; j++)
+//    {
+//      motL.forward(120);
+//      motR.forward(0);
+//    }
+//  }
 ////   if ((irL.isBlack() == 1) && (irR.isBlack() == 1)) {
 ////       motL.forward(left_mot_fwd_speed);
 ////    motR.forward(right_mot_fwd_speed);
@@ -86,7 +122,7 @@ void turnLeft() {
   kickLeft();
   while (turnCounter < 2) {
     motL.forward(0);
-    motR.forward(120);
+    motR.forward(right_mot_fwd_speed);
     if (irL.isBlack()&& !irR.isBlack()) {
       kickLeft();
       turnCounter++;
@@ -97,34 +133,34 @@ void turnLeft() {
 void kickLeft() {
   while (irL.isBlack()) {
     motL.forward(0);
-    motR.forward(120);
+    motR.forward(right_mot_fwd_speed);
   }
 }
 
-void turnRight() {
-  int turnCounter = 0;
-  kickRight();
-  while (turnCounter < 2) {
-    motR.forward(0);
-    motL.forward(70);
-    if (irR.isBlack()&& !irL.isBlack()) {
-      kickRight();
-      turnCounter++;
-    }
-  }
-}
-
-void kickRight() {
-  while (irR.isBlack()) {
-    motR.forward(0);
-    motL.forward(70);
-  }
-}
-
-void stop() {
-  for (long i = 0; i < 20000; i++) {
-    motL.halt();
-    motR.halt();
-  }
-}
+//void turnRight() {
+//  int turnCounter = 0;
+//  kickRight();
+//  while (turnCounter < 2) {
+//    motR.forward(0);
+//    motL.forward(right_mot_fwd_speed);
+//    if (irR.isBlack()&& !irL.isBlack()) {
+//      kickRight();
+//      turnCounter++;
+//    }
+//  }
+//}
+//
+//void kickRight() {
+//  while (irR.isBlack()) {
+//    motR.forward(0);
+//    motL.forward(right_mot_fwd_speed);
+//  }
+//}
+//
+//void stop() {
+//  for (long i = 0; i < 20000; i++) {
+//    motL.halt();
+//    motR.halt();
+//  }
+//}
 
