ebd49cc86de6fcd74bb8416ad9aa4705de42d8ab
Youssef Zayed <yzaye1@ocdsb.ca>
2018-05-21 16:55:06 -0400

    tests

--- Scratch1/Scratch1.ino
+++ Scratch1/Scratch1.ino
@@ -235,17 +235,18 @@ void setNodes()
       if (x + 4 < 28) {
         graph[x][x+4] = 1;
       }
-       if (x + 1 < 28) {
+       if (x + 1 < 28 && !((x+1) % 4 == 0)  ) {
         graph[x][x+1] = 1;
       }
        if (x - 4 >= 0 ) {
         graph[x][x - 4] = 1;
       }
-        if (x - 1 >= 0 ) {
+        if (x - 1 >= 0  && !(x % 4 == 0) ) {
         graph[x][x - 1] = 1;
       }
     
   }
+
   dijkstra(graph, 0);
 }
 
